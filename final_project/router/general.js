cb283b9917ce101ef7ddd77a1ac555d6a300147b
Mihaela-Enescu <Mihaela-Andreea.Enescu@unicredit.eu>
2025-07-22 11:21:56 -0400

    add changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,47 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Send JSON response with formatted books data
+    res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_books = Object.values(books).filter((book) => book.author === author);
+  
+  if (filtered_books.length > 0) {
+    res.send(JSON.stringify(filtered_books,null,4));
+  } else {
+    res.send('Unable to find a book of this author.')
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_books = Object.values(books).filter((book) => book.title === title);
+    
+    if (filtered_books.length > 0) {
+      res.send(JSON.stringify(filtered_books[0],null,4));
+    } else {
+      res.send('Unable to find a book with this title.')
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn].reviews,null,4));
 });
 
 module.exports.general = public_users;
